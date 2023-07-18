bb733ebea80dbce023830e2e9cfe57fa4ad1a0c1
srdhr1234 <srdhr1234@gmail.com>
2023-07-18 12:06:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Function to search and retrieve matching entries
+def search_entries(search_term):
+    search_term = search_term.lower()
+    df['SAP ID'] = df['SAP ID'].astype(str)  # Convert 'SAP ID' column to string type
+    matching_entries = df[(df['SAP ID'].str.lower().str.contains(search_term)) |
+                          (df['Student Name'].str.lower().str.contains(search_term))]
+    return matching_entries.to_html(index=False)
+
+
+# Load the Excel file into a pandas DataFrame
+df = pd.read_excel('Higher_Studies.xlsx')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_term = request.form['search_term']
+    results = search_entries(search_term)
+    return render_template('results.html', results=results)
+
+if __name__ == '__main__':
+    app.run()
